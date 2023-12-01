555f64d74b994ec3b8d31f061a61d2c4a138cd0c
Thomas van den Berg <thomasvdb2002@protonmail.com>
2023-12-01 10:10:36 +0100

    feat-cli: add workable arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Arg, ArgAction, Command, Parser};
+
+/// Just a simple CLI tool to keep track of hours for my work.
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Begin a new task
+    #[arg(short, long, required_unless_present="end")]
+    begin: bool,
+
+    /// End the current task
+    #[arg(short, long)]
+    end: bool,
+
+    /// Specify the issue
+    #[arg(short, long)]
+    issue: String,
+
+    /// Add a more detailed description
+    #[arg(short, long)]
+    description: Option<String>
+}
+
 fn main() {
+    let args = Args::parse();
     println!("Hello, world!");
 }
